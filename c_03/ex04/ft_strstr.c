396b2d45667202ae679929e67ca144dcc6db227a
Javid Khasizada <jkhasiza@c2r6p1.42vienna.com>
2023-06-08 16:25:55 +0200

    Add: ex04

---
+++ c_03/ex04/ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 15:27:11 by jkhasiza          #+#    #+#             */
+/*   Updated: 2023/06/08 16:25:37 by jkhasiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (to_find[j] == str[i])
+		{
+			while (to_find[j] != '\0')
+			{
+				if (str[i + j] != to_find[j])
+					break ;
+				j++;
+			}
+			if (to_find[j] == '\0')
+				return (&str[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
