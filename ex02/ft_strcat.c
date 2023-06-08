3436ce95c3f73cf330bcdaf589e85ba580c12554
Javid Khasizada <jkhasiza@c2r6p1.42vienna.com>
2023-06-08 15:22:06 +0200

    Add: ex02

---
+++ ex02/ft_strcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 14:55:01 by jkhasiza          #+#    #+#             */
+/*   Updated: 2023/06/08 15:04:17 by jkhasiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+
+char *ft_strcat(char *dest, char *src)
+{
+	int j;
+	int i;
+
+	j = ft_strlen(dest);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	dest[j] = '\0';
+
+	return dest;
+}
