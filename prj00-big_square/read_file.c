bf0e2ebdd9529eccaf651defba2a69a0b471fa43
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2023-06-13 14:52:23 +0200

    Prj big square arthur (#5)
    * Add initial makefile (#1) (#2)
    
    * Add initial makefile
    
    * Fix program name
    
    ---------
    
    Co-authored-by: Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
    Co-authored-by: Javid Khasizada <jkhasiza@c2r6p6.42vienna.com>
    
    * Add reading and validating map logic
    
    * fixed leaks
    
    * Fix norminette
    
    ---------
    
    Co-authored-by: Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
    Co-authored-by: Javid Khasizada <jkhasiza@c2r6p6.42vienna.com>
    Co-authored-by: Arthur Bied-Charreton <abied-ch@c2r6p5.42vienna.com>

--- prj00-big_square/read_file.c
+++ prj00-big_square/read_file.c
@@ -15,6 +15,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void	ft_putstr(char *str);
+
 char	*ft_realloc(char *old, int old_size, int new_size)
 {
 	char	*new;
@@ -33,40 +35,48 @@ char	*ft_realloc(char *old, int old_size, int new_size)
 	return (new);
 }
 
-char	*read_file(char *file)
+
+char	*read_file(int fd)
 {
-	int		fd;
 	int		i;
 	char	*str;
 	char	current;
-	int		allocated_size;
+	char	*new;
 
-	allocated_size = 10;
 	i = 0;
-	str = malloc(sizeof(char) * allocated_size + 1);
+	str = malloc(sizeof(char) * 11);
 	if (str == NULL)
 		return (str);
-	fd = open(file, O_RDONLY);
 	while (read(fd, &current, 1) != 0)
 	{
 		str[i] = current;
-		i++;
-		printf("i=%d\n", i);
-		if (i == allocated_size)
+		if (++i % 10 == 0)
 		{
-			allocated_size = allocated_size + 10;
-			char *new = ft_realloc(str, i + 1, sizeof(char) * allocated_size + 1);
+			new = ft_realloc(str, i + 1, sizeof(char) * i + 11);
 			if (new != NULL)
 				str = new;
 			else
-			{
-				free(str);
-				return (new);
-			}
+				return (str);
 		}
 	}
-	printf("i=%d, size=%lu\n", i, sizeof(str));
-	// TODO Close it, it's cold
 	str[i] = '\0';
 	return (str);
+}
+
+char	*open_and_read(char *file)
+{
+	int		fd;
+	char	*str;
+
+	fd = open(file, O_RDONLY);
+	if (fd != -1)
+	{
+		str = read_file(fd);
+	
+		if (close(fd) == -1)
+			ft_putstr("file error");
+		return (str);
+	}
+	ft_putstr("file error");
+	exit (0);
 }
