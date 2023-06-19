67d316e159e1ea3e5450acaa401998840d495f30
ekavajh <javid.khasizada@ericsson.com>
2023-06-19 13:47:41 +0200

    Fix array creation logic from inputs

--- rush_01/write_input.c
+++ rush_01/write_input.c
@@ -11,17 +11,20 @@
 /* ************************************************************************** */
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void	print_lines(int **array, int size);
 int		ft_calculate(int **board, int size);
 
-int	**alloc_array(int size)
+int	**allocate_array(int size)
 {
 	int	**array;
 	int	i;
 
 	i = 0;
 	array = malloc(sizeof(int *) * size);
+	if (array == NULL)
+		return NULL;
 	while (i < size)
 	{
 		array[i] = malloc(sizeof(int) * size);
@@ -30,7 +33,7 @@ int	**alloc_array(int size)
 	return (array);
 }
 
-void	populate_zero(int **array, int size)
+void	populate_initial_values(int **array, int size)
 {
 	int	b;
 	int	k;
@@ -48,25 +51,30 @@ void	populate_zero(int **array, int size)
 	}
 }
 
-void	populate_array(int **array, int *base, int size)
+void	populate_inputs(int **array, int *base, int size)
 {
 	int	b;
 	int	k;
 
 	b = 0;
-	k = 1;
+	int size_of_map = size - 2;
 	while (base[b])
 	{	
-		while (b < size - 2)
+		// populate left column
+		k = 1;
+		while (b < size_of_map)
 			array[k++][0] = base[b++];
+		// populate right column
 		k = 1;
-		while ((size - 2) <= b && b < (size - 2) * 2)
+		while ((size_of_map) <= b && b < (size_of_map) * 2)
 			array[k++][size - 1] = base[b++];
+		// populate top row
 		k = 1;
-		while ((size - 2) * 2 <= b && b < (size - 2) * 3)
+		while ((size_of_map) * 2 <= b && b < (size_of_map) * 3)
 			array[0][k++] = base[b++];
+		// populate bottom row
 		k = 1;
-		while ((size - 2) * 3 <= b && b < (size - 2) * 4)
+		while ((size_of_map) * 3 <= b && b < (size_of_map) * 4)
 			array[size - 1][k++] = base[b++];
 	}
 }
@@ -81,58 +89,52 @@ void	free_array(int **array, int size)
 		free(array[i]);
 		i++;
 	}
+	free(array);
+}
+
+int	**get_populated_array(int *base, int size)
+{
+	int	**array;
+	
+	array = allocate_array(size);
+	if (array == NULL)
+		return NULL;
+	populate_initial_values(array, size);
+	populate_inputs(array, base, size);
+
+	return (array);
 }
 
 int	write_input(int *base, int size)
 {
 	int	**array;
 
-	array = alloc_array(size);
-	populate_zero(array, size);
-	populate_array(array, base, size);
+	array = get_populated_array(base, size);
+	if (array == NULL)
+		return (0);
+
+	printf("Before CALCULATION\n\n");
+	print_lines(array, size);
 	if (ft_calculate(array, size) == 0)
 	{
 		free_array(array, size);
 		return (0);
 	}
+	printf("After CALCULATION\n\n");
 	print_lines(array, size);
 	free_array(array, size);
 	return (1);
 }
-/*
-int    write_input(int *base, int size)
-{
-	int	**array;
-	int	i;
-	int	b;
-	int	k;
-	
-	i = 0;
-	array = malloc(sizeof(int *) * (size));
-	while (i < size)
-		array[i++] = malloc(sizeof(int) * (size)); 
-	k=1;
-	b = 0;
-	while (base[b])
-	{
-		while (b < ((size-2)))
-			array[k++][0] = base[b++];
-		k = 1;	
-		while ((size-2) <= b && b < ((size-2)*2))
-			array[k++][size-1] = base[b++];	
-		k = 1;		
-		while(((size-2)*2) <= b && b < ((size-2)*3))
-			array[0][k++] = base[b++];
-		k = 1;
-		while ((((size-2)*3)) <= b && b < (size-2)*4)
-			array[size-1][k++] = base[b++];
 
-	}
-	if (ft_calculate(array, size) == 0)
-		return (0);
-	print_lines(array, size);
-	i = 0;
-	while (i < size)
-		free (array[i++]);
-	return (1);
-}*/
+/*
+
+    3 3 2 1
+    | | | |
+4 - 1 2 3 4 - 1
+3 - 2 3 4 1 - 2
+1 - 4 1 2 3 - 2
+2 - 3 4 1 2 - 2
+	| | | |
+	2 1 3 3
+
+*/
