2501f801ff04a726ea9a40e34cfc3a9c74c2cd88
Javid Khasizada <jkhasiza@c2r5p14.42vienna.com>
2023-09-15 12:47:07 +0200

    Fixed ft_memcpy structure

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 12:02:57 by jkhasiza          #+#    #+#             */
-/*   Updated: 2023/09/15 12:31:14 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2023/09/15 12:45:06 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 	char	*c_src;
