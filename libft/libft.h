fb6c40ae828c25d00c457eb8e6cf3413987a7182
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-09-15 15:20:18 +0000

    Add ft_tolower

--- libft/libft.h
+++ libft/libft.h
@@ -22,6 +22,7 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strlen(char *str);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+int     ft_tolower(int c);
 int     ft_toupper(int c);
 
 #endif
