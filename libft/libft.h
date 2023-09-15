807d0a2389b3e31572dd64ba28e9594819fa7c9b
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-09-15 14:59:45 +0200

    Add ft_memchr (#24)

--- libft/libft.h
+++ libft/libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <stdio.h>
 
 void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(char c);
@@ -10,6 +11,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(char c);
 char	*ft_itoa(int n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	**ft_split(char const *s, char c);
