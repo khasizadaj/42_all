8eba8f1f399e166959f24c2e4d7d4baa5c05b7e8
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-09-15 12:48:07 +0200

    Add ft_memcpy (#22)

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 12:02:57 by jkhasiza          #+#    #+#             */
+/*   Updated: 2023/09/15 12:45:06 by jkhasiza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*c_src;
+	char	*c_dest;
+
+	c_src = (char *) src;
+	c_dest = (char *) dest;
+	i = 0;
+	while (i < n)
+	{
+		c_dest[i] = c_src[i];
+		i++;
+	}
+	return (dest);
+}
