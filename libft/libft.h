86daccafafa6865ea79800feb46162950e1e59c4
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-09-16 18:43:06 +0000

    Fix norm issue in libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 10:47:39 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/16 18:34:09 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/16 18:41:41 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@
 # include <ctype.h>
 # include <fcntl.h>
 
-int             ft_atoi(const char *nptr);
+int				ft_atoi(const char *nptr);
 void			ft_bzero(void *s, size_t n);
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
-int             ft_isprint(int c);
-int             ft_isspace(int c);
+int				ft_isprint(int c);
+int				ft_isspace(int c);
 void			*ft_calloc(size_t nmemb, size_t size);
 char			*ft_itoa(int n);
 void			*ft_memchr(const void *s, int c, size_t n);
@@ -49,7 +49,7 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 char			*ft_strdup(const char *s);
 char			*ft_strrchr(const char *s, int c);
-size_t          ft_strlen(const char *s);
+size_t			ft_strlen(const char *s);
 char			*ft_strtrim(char const *s1, char const *set);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 int				ft_tolower(int c);
