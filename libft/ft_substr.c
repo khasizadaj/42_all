a84fbdfcba8d07b84513f73610180a14497d03a9
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-09-16 15:35:55 +0000

    Fix ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,28 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 16:46:07 by jkhasiza          #+#    #+#             */
-/*   Updated: 2023/09/15 10:48:39 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2023/09/16 15:25:00 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	i;
+	size_t	length;
 
-	i = 0;
-	sub = malloc(sizeof(char) * len);
+	length = (size_t) ft_strlen((char *) s);
+	if (len > length)
+		len = length;
+	if (start >= length || length == 0)
+		len = 0;
+	if (len > length - start)
+		len = length - start;
+	sub = malloc(len + 1);
 	if (sub == NULL)
 		return (NULL);
-	while (i < len && s[start + i] != '\0')
-	{
+	i = -1;
+	while (++i < len)
 		sub[i] = s[start + i];
-		i++;
-	}
+	sub[i] = '\0';
 	return (sub);
 }
+	// if (start >= length || length == 0 || len == 0)
+	// {
+	// 	sub = malloc(1);
+	// 	if (sub == NULL)
+	// 		return (NULL);
+	// }
+	// else
+	// {
+	// 	if (len > length)
+	// 		len = length;
+	// 	sub = malloc(sizeof(char) * (len + 1));
+	// 	if (sub == NULL)
+	// 		return (NULL);
+	// 	while (i < len && s[start + i] != '\0')
+	// 	{
+	// 		sub[i] = s[start + i];
+	// 		i++;
+	// 	}
+	// }
