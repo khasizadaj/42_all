15e8594e09415cf208dccf83113ca448c399ab7f
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-09-29 19:44:19 +0000

    Create headerfile for printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include "./libft/libft.h"
+
+int     chr_in(char c, char const *str);
+int     ft_printf(const char *input, ...);
+char    *strjoin_on_steroids(int n, ...);
+
+
