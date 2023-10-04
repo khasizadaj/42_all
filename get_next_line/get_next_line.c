6208c952e1db35d445145a27ce0d3d46b5ecbc6d
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2023-10-04 20:04:29 +0000

    Add backbone of project

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 17:08:27 by codespace         #+#    #+#             */
+/*   Updated: 2023/10/04 17:33:54 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+
+ == GET NEXT LINE
+
+This project is about programming a function that returns a line
+read from a file descriptor.
+
+*/
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	printf("hi, %d", fd);
+	return ("");
+}
