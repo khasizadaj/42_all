76106a480d878d2c6e5d0a300ed5f8a07f9f1c3b
Javid Khasizada <jkhasiza@c2r3p4.42vienna.com>
2023-10-16 10:59:17 +0200

    Simplify main to focus on get_next_line bug

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -3,61 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:44:53 by codespace         #+#    #+#             */
-/*   Updated: 2023/10/07 12:04:07 by codespace        ###   ########.fr       */
+/*   Updated: 2023/10/16 10:57:13 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "program_utils.h"
 
-typedef struct s_files {
-	char    **file_list;
-	char    **path_list;
-} t_files;
+// typedef struct s_files {
+// 	char    **file_list;
+// 	char    **path_list;
+// } t_files;
 
-int    get_files(t_files *files)
-{
-	char    **file_list;
-	char    **path_list;
+// int    get_files(t_files *files)
+// {
+// 	char    **file_list;
+// 	char    **path_list;
 
-	file_list = ft_calloc(7, sizeof(char *));
-	if (!file_list)
-		return (-1);
-
-	file_list[0] = "file_1.txt";
-	file_list[1] = "file_2.txt";
-	file_list[2] = "file_3.txt";
-	file_list[3] = "file_4.txt";
-	file_list[4] = "file_5.txt";
-	file_list[5] = "file_6.txt";
-	file_list[6] = '\0';
+// 	file_list = malloc(7 * sizeof(char *));
+// 	if (!file_list)
+// 		return (-1);
+// 	ft_bzero(file_list, sizeof(char *) * 7);
+// 	file_list[0] = "file_1.txt";
+// 	file_list[1] = "file_2.txt";
+// 	file_list[2] = "file_3.txt";
+// 	file_list[3] = "file_4.txt";
+// 	file_list[4] = "file_5.txt";
+// 	file_list[5] = "file_6.txt";
+// 	file_list[6] = '\0';
 
 	
-	path_list = ft_calloc(7, sizeof(char *));
-	if (!path_list)
-		return (free(file_list), -1);
+// 	path_list = malloc(7 * sizeof(char *));
+// 	if (!path_list)
+// 		return (free(file_list), -1);
+// 	ft_bzero(path_list, sizeof(char *) * 7);
 
-	path_list[0] = "../test/get_next_line/file_1.txt";
-	path_list[1] = "../test/get_next_line/file_2.txt";
-	path_list[2] = "../test/get_next_line/file_3.txt";
-	path_list[3] = "../test/get_next_line/file_4.txt";
-	path_list[4] = "../test/get_next_line/file_5.txt";
-	path_list[5] = "../test/get_next_line/file_6.txt";
-	path_list[6] = '\0';
+// 	path_list[0] = "../test/get_next_line/file_1.txt";
+// 	path_list[1] = "../test/get_next_line/file_2.txt";
+// 	path_list[2] = "../test/get_next_line/file_3.txt";
+// 	path_list[3] = "../test/get_next_line/file_4.txt";
+// 	path_list[4] = "../test/get_next_line/file_5.txt";
+// 	path_list[5] = "../test/get_next_line/file_6.txt";
+// 	path_list[6] = '\0';
 	
-	files->file_list = file_list;
-	files->path_list = path_list;
-	return (1);
-}
+// 	files->file_list = file_list;
+// 	files->path_list = path_list;
+// 	return (1);
+// }
 
 int main(int argc, char const *argv[])
 {
 	int     fd;
 	char    *line;
-	t_files *files;
+	// t_files *files;
 	int     file_id;
 
     if (argc < 2) {
@@ -67,16 +68,16 @@ int main(int argc, char const *argv[])
 
 	// TODO: Read multiple args
 	file_id = ft_atoi(argv[1]) - 1;
-	printf("FILE_ID=%d\n", file_id);
-	files = ft_calloc(1, sizeof(t_files));
-	if (!files)
-		return (1);
+	// printf("FILE_ID=%d\n", file_id);
+	// files = malloc(sizeof(t_files));
+	// if (!files)
+	// 	return (1);
+	// printf("Hello");
+	// if (!get_files(files))
+	// 	return (free(files->path_list), free(files->file_list), free(files), 1);
 
-	if (!get_files(files))
-		return (free(files->path_list), free(files->file_list), free(files), 1);
-
-	printf("[o]\tpath_list[%d]='%s']\n", file_id, files->path_list[file_id]);
-	fd = open(files->path_list[file_id], O_RDONLY);
+	// printf("[o]\tpath_list[%d]='%s']\n", file_id, files->path_list[file_id]);
+	fd = open("../test/get_next_line/file_5.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Error number: %d\n", errno);
@@ -90,7 +91,7 @@ int main(int argc, char const *argv[])
 		line = get_next_line(fd);
 		printf("[main]\t[l=%ld]\t'%s'\n", ft_strlen(line), line);
 		if (line == NULL)
-			return (free(files->path_list), free(files->file_list), free(files), 1);
+			return (1);
 		printf("\nLINES READ: %d\n\n\n", i + 1);
 		free(line);
 	}
@@ -99,5 +100,5 @@ int main(int argc, char const *argv[])
 		perror("Program");
 		exit(1);
 	}
-	return (free(files->path_list), free(files->file_list), free(files), 0);
+	return (0);
 }
