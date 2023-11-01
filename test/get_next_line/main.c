f46d03e2995a4728da62558a2777c2875240d25a
Javid Khasizada <jkhasiza@c4r7p10.42vienna.com>
2023-11-01 14:53:35 +0100

    Make main.c for GNL simpler

--- test/get_next_line/main.c
+++ test/get_next_line/main.c
@@ -3,81 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkhasizada <jkhasizada@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:44:53 by codespace         #+#    #+#             */
-/*   Updated: 2023/10/17 19:04:33 by jkhasizada       ###   ########.fr       */
+/*   Updated: 2023/11/01 14:53:00 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "program_utils.h"
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
 
-// typedef struct s_files {
-// 	char    **file_list;
-// 	char    **path_list;
-// } t_files;
-
-// int    get_files(t_files *files)
-// {
-// 	char    **file_list;
-// 	char    **path_list;
-
-// 	file_list = malloc(7 * sizeof(char *));
-// 	if (!file_list)
-// 		return (-1);
-// 	ft_bzero(file_list, sizeof(char *) * 7);
-// 	file_list[0] = "file_1.txt";
-// 	file_list[1] = "file_2.txt";
-// 	file_list[2] = "file_3.txt";
-// 	file_list[3] = "file_4.txt";
-// 	file_list[4] = "file_5.txt";
-// 	file_list[5] = "file_6.txt";
-// 	file_list[6] = '\0';
-
-	
-// 	path_list = malloc(7 * sizeof(char *));
-// 	if (!path_list)
-// 		return (free(file_list), -1);
-// 	ft_bzero(path_list, sizeof(char *) * 7);
-
-// 	path_list[0] = "../test/get_next_line/file_1.txt";
-// 	path_list[1] = "../test/get_next_line/file_2.txt";
-// 	path_list[2] = "../test/get_next_line/file_3.txt";
-// 	path_list[3] = "../test/get_next_line/file_4.txt";
-// 	path_list[4] = "../test/get_next_line/file_5.txt";
-// 	path_list[5] = "../test/get_next_line/file_6.txt";
-// 	path_list[6] = '\0';
-	
-// 	files->file_list = file_list;
-// 	files->path_list = path_list;
-// 	return (1);
-// }
-
-int main(int argc, char const *argv[])
+int main()
 {
 	int     fd;
 	char    *line;
-	// t_files *files;
-	int     file_id;
 
-    if (argc < 2) {
-        printf("Error: Missing argument.\n");
-        return (-1);
-    }
-
-	// TODO: Read multiple args
-	file_id = ft_atoi(argv[1]) - 1;
-	// printf("FILE_ID=%d\n", file_id);
-	// files = malloc(sizeof(t_files));
-	// if (!files)
-	// 	return (1);
-	// printf("Hello");
-	// if (!get_files(files))
-	// 	return (free(files->path_list), free(files->file_list), free(files), 1);
-
-	// printf("[o]\tpath_list[%d]='%s']\n", file_id, files->path_list[file_id]);
-	fd = open("../test/get_next_line/file_5.txt", O_RDONLY);
+	fd = open("files/file_5.txt", O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Error number: %d\n", errno);
@@ -89,7 +32,7 @@ int main(int argc, char const *argv[])
 	while (++i < 100)
 	{
 		line = get_next_line(fd);
-		printf("[main]\t[l=%ld]\t'%s'\n", ft_strlen(line), line);
+		printf("[main]\t[l=%ld]\t'%s'\n", ft_strlen(line, '\0'), line);
 		if (line == NULL)
 			return (1);
 		printf("\nLINES READ: %d\n\n\n", i + 1);
