b575cec334bd63a281a387856e2cc8405e7dd4fe
khasizadaj <javid.khasizada@gmail.com>
2023-12-17 19:57:25 +0100

    Remove comments

--- push_swap/test/main.c
+++ push_swap/test/main.c
@@ -1,6 +1,5 @@
 #include "../include/push_swap.h"
 
-// Helper function to print the linked list
 void print_list(t_number *stack)
 {
     while (stack != NULL)
@@ -13,7 +12,6 @@ void print_list(t_number *stack)
 
 void test_reverse_a_with_multiple_numbers()
 {
-    // Create a sample linked list for testing
     t_number *node1 = malloc(sizeof(t_number));
     t_number *node2 = malloc(sizeof(t_number));
     t_number *node3 = malloc(sizeof(t_number));
@@ -35,14 +33,11 @@ void test_reverse_a_with_multiple_numbers()
     printf("Before reverse_a:\n");
     print_list(node1);
 
-    // Call the swap_a function
     reverse_a(&node1);
 
     printf("After reverse_a:\n");
     print_list(node1);
 
-    // Add assertions to check if the swap was successful
-    // You can use an assertion library for more robust testing
     if (node1->number == 2 && node1->next->number == 3 && node1->next->next->next->number == 1)
     {
         printf("PASS: reverse_a test passed!\n\n");
@@ -55,7 +50,6 @@ void test_reverse_a_with_multiple_numbers()
 
 void test_reverse_a_with_3_numbers()
 {
-    // Create a sample linked list for testing
     t_number *node1 = malloc(sizeof(t_number));
     t_number *node2 = malloc(sizeof(t_number));
     t_number *node3 = malloc(sizeof(t_number));
@@ -73,14 +67,11 @@ void test_reverse_a_with_3_numbers()
     printf("Before reverse_a:\n");
     print_list(node1);
 
-    // Call the swap_a function
     reverse_a(&node1);
 
     printf("After reverse_a:\n");
     print_list(node1);
 
-    // Add assertions to check if the swap was successful
-    // You can use an assertion library for more robust testing
     if (node1->number == 2 && node1->next->number == 3 && node1->next->next->number == 1)
     {
         printf("PASS: reverse_a test passed!\n\n");
@@ -93,7 +84,6 @@ void test_reverse_a_with_3_numbers()
 
 void test_swap_a_with_1_number()
 {
-    // Create a sample linked list for testing
     t_number *node1 = malloc(sizeof(t_number));
     
     node1->number = 1;
@@ -103,15 +93,11 @@ void test_swap_a_with_1_number()
     printf("Before swap_a:\n");
     print_list(node1);
 
-    // Call the swap_a function
     swap_a(&node1);
 
     printf("After swap_a:\n");
     print_list(node1);
 
-    // Add assertions to check if the swap was successful
-    // You can use an assertion library for more robust testing
-    if (node1->number == 1 && node1->next == NULL)
     {
         printf("PASS: swap_a test passed!\n\n");
     }
@@ -123,7 +109,6 @@ void test_swap_a_with_1_number()
 
 void test_swap_a_with_2_numbers()
 {
-    // Create a sample linked list for testing
     t_number *node1 = malloc(sizeof(t_number));
     t_number *node2 = malloc(sizeof(t_number));
     
@@ -136,14 +121,11 @@ void test_swap_a_with_2_numbers()
     printf("Before swap_a:\n");
     print_list(node1);
 
-    // Call the swap_a function
     swap_a(&node1);
 
     printf("After swap_a:\n");
     print_list(node1);
 
-    // Add assertions to check if the swap was successful
-    // You can use an assertion library for more robust testing
     if (node1->number == 2 && node1->next->number == 1)
     {
         printf("PASS: swap_a test passed!\n\n");
