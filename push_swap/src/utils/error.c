dd29bc918919a0874d25209d9ab9b9116ee7d07e
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-01-14 13:17:01 +0000

    Norm error.c

--- push_swap/src/utils/error.c
+++ push_swap/src/utils/error.c
@@ -6,15 +6,15 @@
 /*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 16:56:35 by codespace         #+#    #+#             */
-/*   Updated: 2024/01/06 13:00:03 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2024/01/14 13:16:40 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-void exit_for(int reason)
+void	exit_for(int reason)
 {
-    if (reason == INVALID_INPUT)
-        ft_printf("Input is not valid.\n");
-    exit(reason);
+	if (reason == INVALID_INPUT)
+		ft_printf("Input is not valid.\n");
+	exit(reason);
 }
