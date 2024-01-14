831406917d7591fa856c2002f30a62e27e94b6d9
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-01-14 13:28:09 +0000

    Norm alg.h

--- push_swap/src/alg/alg.h
+++ push_swap/src/alg/alg.h
@@ -6,21 +6,24 @@
 /*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 19:30:26 by jkhasiza          #+#    #+#             */
-/*   Updated: 2024/01/13 20:12:47 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2024/01/14 13:27:47 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/push_swap.h"
 
 #ifndef ALG_H
 # define ALG_H
+
+# include "../../include/push_swap.h"
 
 int		*get_cheapest(t_number *from, t_number *to, bool reverse);
 lli		get_smallest(t_number *stack);
 lli		get_largest(t_number *stack);
 
-void	get_steps_to_top_at_to(int lookup, int *steps, t_number *stack, char direction);
-void	get_steps_to_top_at_from(int lookup, int *steps, t_number *stack, char direction);
+void	get_steps_to_top_at_to(int lookup, int *steps, t_number *stack,
+			char direction);
+void	get_steps_to_top_at_from(int lookup, int *steps, t_number *stack,
+			char direction);
 lli		get_location_to_move_reverse(lli val, t_number *to);
 lli		get_location_to_move(lli val, t_number *to);
 
