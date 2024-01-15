323b0c71417230395fcb197705eace5eed0cd371
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-01-15 18:06:22 +0000

    Allocate after valid input

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 18:57:52 by jkhasiza          #+#    #+#             */
-/*   Updated: 2024/01/15 18:01:52 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2024/01/15 18:05:29 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		exit_for(NO_INPUT);
+	if (!validate_input(argc, argv))
+	{
+		free(data);
+		exit_for(INVALID_INPUT);
+	}
 	data = malloc(sizeof(t_data));
 	if (!data)
 		return (1);
 	data->size = 5;
 	data->stack_a = NULL;
 	data->stack_b = NULL;
-	if (!validate_input(argc, argv))
-	{
-		free(data);
-		exit_for(INVALID_INPUT);
-	}
 	generate_stack(data, argc, argv);
 	sort(data);
 	ft_stackclear(&data->stack_a);
