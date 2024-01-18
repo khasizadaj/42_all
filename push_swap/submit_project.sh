e08fe3b01a74e87d4b596aa93500e8a7adab7e0c
khasizadaj <javid.khasizada@gmail.com>
2024-01-18 18:43:00 +0100

    Update workspace + submission script

--- push_swap/submit_project.sh
+++ push_swap/submit_project.sh
@@ -1,4 +1,21 @@
-DEST="../../cursus/push_swap-V1.0"
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+	echo "Error: At least 2 positional arguments are required."
+	echo "Usage: $0 prj_name version"
+fi
+
+PROJECT=$1
+VERSION=$2
+
+DEST="$HOME/cave/cursus/$PROJECT-$VERSION"
+
+echo $DEST
+
+if ! test -d $DEST; then
+	echo "Folder doesn't exist. Please, clone your project."
+	exit 1
+fi
 
 cp -r ./include $DEST
 cp ./Makefile $DEST
