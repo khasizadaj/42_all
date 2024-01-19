d4ad31485b88405b103fed86cdbc924d080acfe6
khasizadaj <javid.khasizada@gmail.com>
2024-01-19 20:54:53 +0100

    Fix  file extension issue

--- soo__loong/src/main.c
+++ soo__loong/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jkhasiza <jkhasiza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 20:30:10 by jkhasiza          #+#    #+#             */
-/*   Updated: 2024/01/18 21:57:01 by jkhasiza         ###   ########.fr       */
+/*   Updated: 2024/01/19 20:45:08 by jkhasiza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		exit_for(INVALID_MAP_NO_MAP);
-	if (!ft_str_has(argv[1], MAP_EXTENSION))
+	if (!ft_str_endswith(argv[1], MAP_EXTENSION))
 		exit_for(INVALID_MAP_WRONG_FILE_TYPE);
 
 	ft_printf("ALL GOOHUDEE");
